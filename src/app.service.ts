6fac65429e3b65f1a956cd129d1292400b5524fe
Jeetanshu Srivastava <92146359+Jeetanshu18@users.noreply.github.com>
2024-09-19 11:29:13 +0530

    Updated app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Updated Github CI Test';
+    return 'Updated Github CI Workflow Test';
   }
 }
